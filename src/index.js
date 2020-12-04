cbd2668b743773691435d046528df39b2613344b
Andrew Davis <atd285@gmail.com>
2020-12-03 20:27:07 -0500

    connect with twitter backend

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 const fs = require('fs');
+const { TwitterClient } = require('twitter-api-client');
+
+require('dotenv').config();
+
+const {
+  TWITTER_API_KEY: apiKey,
+  TWITTER_API_SECRET: apiSecret,
+  TWITTER_ACCESS_TOKEN: accessToken,
+  TWITTER_TOKEN_SECRET: accessTokenSecret,
+} = process.env;
+
+const twitterClient = new TwitterClient({
+  apiKey,
+  apiSecret,
+  accessToken,
+  accessTokenSecret,
+});
